fdd8fbfa7769c7fac70c60a35eab15955501a7a4
adam+mario <adam+mario@appacademy.io>
2013-02-18 10:54:02 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GistClone::Application.config.secret_token = 'e72e759268453bbb91326abb6e80499a6ec1e594601ccbdd5966d7019aee5dd5d8062165c2d520c9c0bbde7633b2c31d0e374a2a14df22daecc71713c386a2dd'
